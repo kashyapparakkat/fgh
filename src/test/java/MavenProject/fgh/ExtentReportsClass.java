2caada5b8c95f1999cdb39a90a8f1338da0c14f2
kashyapparakkat <32475791+kashyapparakkat@users.noreply.github.com>
2017-10-03 16:46:04 +0530

    hi

--- src/test/java/MavenProject/fgh/ExtentReportsClass.java
+++ src/test/java/MavenProject/fgh/ExtentReportsClass.java
@@ -21,7 +21,9 @@ public class ExtentReportsClass {
 		//replaceExisting - Setting to overwrite (TRUE) the existing file or append to it
 		//True (default): the file will be replaced with brand new markup, and all existing data will be lost. Use this option to create a brand new report
 		//False: existing data will remain, new tests will be appended to the existing report. If the the supplied path does not exist, a new file will be created.
-		extent = new ExtentReports (System.getProperty("user.dir") +"/test-output/STMExtentReport.html", true);
+		File file = new File(System.getProperty("user.dir") +"/Reports");
+		file.mkdir();
+		extent = new ExtentReports (file.getAbsolutePath()+"/STMExtentReport.html_"+System.currentTimeMillis(), true);
 		//extent.addSystemInfo("Environment","Environment Name")
 		extent
                 .addSystemInfo("Host Name", "SoftwareTestingMaterial")
